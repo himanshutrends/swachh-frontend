8f6363911f45c450b01149b3b2f1c4a1238d27bf
himanshutrends <himanshu24ai018@satiengg.in>
2024-06-29 15:55:28 +0530

    added report page and ui components

---
+++ src/app/report/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "@/components/ui/header";
+import ReportCard from "@/components/reportCard";
+
+export default function Report() {
+    return (
+        <div>
+        <Header />
+        <ReportCard />
+        </div>
+    );
+}
---
+++ src/components/reportCard.js
@@ -0,0 +1,99 @@
+
+export default function ReportCard() {
+    return (
+        <section className="w-full h-screen bg-white flex flex-col items-center justify-center px-4">
+            <div className="border-[1px] rounded-lg shadow-sm hover:shadow-lg p-5 max-w-sm w-full text-gray-600 space-y-5">
+                <div className="text-center pb-8">
+                    <div className="mt-5">
+                        <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Report for Collection</h3>
+                    </div>
+                </div>
+                <form
+                    className="space-y-5"
+                >
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            required
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="number"
+                            required
+                            placeholder="Phone"
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                        />
+                    </div>
+                    <div className="relative">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#9ca3af " strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" stroke="#9ca3af " strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g>
+                        </svg>
+                        <input
+                            type="text"
+                            placeholder="Address"
+                            className="w-full py-2 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                        />
+                    </div>
+
+                    <div className="relative mx-auto mt-12">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="absolute top-0 bottom-0 w-5 h-5 my-auto text-gray-400 right-3"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2">
+                            <option>Select Dustbin Fill Level</option>
+                            <option>50% (Medium)</option>
+                            <option>75% (High)</option>
+                            <option>100% (Full)</option>
+                        </select>
+                    </div>
+
+                    <button
+                        className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                    >
+                        Report
+                    </button>
+                </form>
+
+                <p className="text-center">Already Repoted? <a href="javascript:void(0)" className="font-medium text-indigo-600 hover:text-indigo-500">Check Status</a></p>
+            </div>
+
+            <div className="mt-12 mx-4 px-4 rounded-md border-l-4 border-green-500 bg-green-50 md:max-w-2xl md:mx-auto md:px-8">
+                <div className="flex justify-between py-3">
+                    <div className="flex">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 rounded-full text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                        <div className="self-center ml-3">
+                            <span className="text-green-600 font-semibold">
+                                Success
+                            </span>
+                            <p className="text-green-600 mt-1">
+                                Report Submitted Successfully <br />
+                                <b>Report ID: 123456</b>
+                            </p>
+                        </div>
+                    </div>
+                    <button className="self-start text-green-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
