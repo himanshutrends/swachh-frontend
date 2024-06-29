e337b2736526f030c13b6fb3a6d6b3668985b3ef
himanshutrends <himanshu24ai018@satiengg.in>
2024-06-29 13:58:46 +0530

    edited signup

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -2,58 +2,47 @@ import Image from "next/image";
 
 export default function Signup() {
     return (
-        <main className="w-full h-screen flex flex-col items-center justify-center bg-white sm:px-4">
-            <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
+        <main className="w-full h-screen flex flex-col items-center justify-center px-4">
+            <div className="max-w-sm w-full text-gray-600">
                 <div className="text-center">
                     <Image src="/swachh-logo.png" width={150} height={30} className="mx-auto" />
                     <div className="mt-5 space-y-2">
-                        <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Create an account</h3>
+                        <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Sign up</h3>
                         <p className="">Already have an account? <a href="javascript:void(0)" className="font-medium text-indigo-600 hover:text-indigo-500">Log in</a></p>
                     </div>
                 </div>
-                <div className="bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg">
-                    <form
-                        className="space-y-5"
-
+                <form
+                    onSubmit={(e) => e.preventDefault()}
+                    className="mt-8 space-y-5"
+                >
+                    <div>
+                        <label className="font-medium">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            required
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                        />
+                    </div>
+                    <div>
+                        <label className="font-medium">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            required
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                        />
+                    </div>
+                    <button
+                        className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
                     >
-                        <div>
-                            <label className="font-medium">
-                                Name
-                            </label>
-                            <input
-                                type="text"
-                                required
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="font-medium">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                required
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="font-medium">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                required
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                            />
-                        </div>
-                        <button
-                            className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-                        >
-                            Create account
-                        </button>
-                    </form>
-                </div>
+                        Create account
+                    </button>
+                </form>
+                
             </div>
         </main>
     )
-}
+    }
